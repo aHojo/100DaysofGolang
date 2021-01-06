fc97ed6d25592fc5176c491dfc71bdd24cc31e62
ahojo <ahojo@upenn.edu>
2021-01-06 14:49:59 -0500

    Going fast because I know this stuff already

---
+++ learngo/src/06-variables/06-project-greeter/exercises/02-print-the-path/main.go
@@ -0,0 +1,35 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Print the Path
+//
+//  Print the path of the running program by getting it
+//  from `os.Args` variable.
+//
+// HINT
+//  Use `go build` to build your program.
+//  Then run it using the compiled executable program file.
+//
+// EXPECTED OUTPUT SHOULD INCLUDE THIS
+//  myprogram
+// ---------------------------------------------------------
+
+func main() {
+
+	program := os.Args[0]
+
+	fmt.Println(program)
+}
